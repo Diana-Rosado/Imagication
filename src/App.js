493517bd2f92daba651dcb1525dc123f0b20d97c
Bryan Pinos <pinosbryana@gmail.com>
2023-06-17 14:50:02 -0400

    Added firebase connection. Get Detailed Quote Button Submission to Database
    On Submit in the Detailed Quote form, the information gets submitted to the firestore database.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import MobileNavbar from './navbar/MobileNavbar.js'
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import './App.css';
+import MobileNavbar from "./navbar/MobileNavbar.js";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import "./App.css";
 import "@fontsource/montserrat";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Landing from './Pages/Landing.js'
-import OurStory from './Pages/OurStory.js';
-import ForSchools from './Pages/ForSchools.js';
-import Pricing from './Pages/Pricing.js';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Landing from "./Pages/Landing.js";
+import OurStory from "./Pages/OurStory.js";
+import ForSchools from "./Pages/ForSchools.js";
+import Pricing from "./Pages/Pricing.js";
+
+import app from "./firebaseConfig.js";
+import { getAnalytics } from "firebase/analytics";
+
+getAnalytics(app);
 
 function App() {
   const theme = createTheme({
     typography: {
       allVariants: {
-        fontFamily: 'montserrat',
+        fontFamily: "montserrat",
       },
     },
   });
@@ -26,24 +28,23 @@ function App() {
       element: <Landing />,
     },
     {
-      path:"/ourstory",
-      element: <OurStory/>
+      path: "/ourstory",
+      element: <OurStory />,
     },
     {
-      path:"/forschools",
-      element: <ForSchools/>
+      path: "/forschools",
+      element: <ForSchools />,
     },
     {
-      path:"/pricing",
-      element: <Pricing/>
-    }
+      path: "/pricing",
+      element: <Pricing />,
+    },
   ]);
 
   return (
-    <ThemeProvider theme={theme} >
-          <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
     </ThemeProvider>
-
   );
 }
 
--- src/Body/Pricing/Form.js
+++ src/Body/Pricing/Form.js
@@ -1,17 +1,51 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
+import app from "../../firebaseConfig.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  serverTimestamp,
+} from "firebase/firestore";
+const db = getFirestore(app);
 
-
+const initialValues = {
+  fullName: "",
+  email: "",
+  phoneNumber: "",
+  schoolName: "",
+};
 
 export default function FormDialog(props) {
+  const [clientInfo, setClientInfo] = useState(initialValues);
 
+  const handleInputChange = (e) => {
+    setClientInfo((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const addToDB = async () => {
+    const colRef = collection(db, "salesQuote");
+    props.countOfItems[0] = props.finalCost;
+    await addDoc(colRef, {
+      fullName: clientInfo.fullName,
+      email: clientInfo.email,
+      phoneNumber: clientInfo.phoneNumber,
+      schoolName: clientInfo.schoolName,
+      estimatedCosts: props.countOfItems,
+      requestedDate: serverTimestamp(),
+    });
+    setClientInfo(initialValues);
+  };
 
   return (
     <div>
@@ -19,18 +53,24 @@ export default function FormDialog(props) {
         <DialogTitle>Detailed Quote</DialogTitle>
         <DialogContent>
           <DialogContentText>
-            After submitting this form, we will be sure to contact you as soon as possible.
+            After submitting this form, we will be sure to contact you as soon
+            as possible.
           </DialogContentText>
           <TextField
+            value={clientInfo.fullName}
+            required
             autoFocus
             margin="dense"
-            id="name"
+            id="fullName"
             label="Full Name"
             type="text"
             fullWidth
             variant="standard"
+            onChange={handleInputChange}
           />
-                    <TextField
+          <TextField
+            value={clientInfo.email}
+            required
             autoFocus
             margin="dense"
             id="email"
@@ -38,29 +78,42 @@ export default function FormDialog(props) {
             type="email"
             fullWidth
             variant="standard"
+            onChange={handleInputChange}
           />
-                              <TextField
+          <TextField
+            value={clientInfo.phoneNumber}
+            required
             autoFocus
             margin="dense"
-            id="number"
+            id="phoneNumber"
             label="Phone Number"
             type="text"
             fullWidth
             variant="standard"
+            onChange={handleInputChange}
           />
-                                        <TextField
+          <TextField
+            value={clientInfo.schoolName}
             autoFocus
             margin="dense"
-            id="school"
+            id="schoolName"
             label="School Name"
             type="text"
             fullWidth
             variant="standard"
+            onChange={handleInputChange}
           />
         </DialogContent>
         <DialogActions>
           <Button onClick={props.handleClose}>Cancel</Button>
-          <Button onClick={props.handleClose}>Submit</Button>
+          <Button
+            onClick={() => {
+              addToDB();
+              props.handleClose();
+            }}
+          >
+            Submit
+          </Button>
         </DialogActions>
       </Dialog>
     </div>
