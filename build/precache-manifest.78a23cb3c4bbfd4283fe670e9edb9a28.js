6ee482acb429d04029cabc1cbabb17200a2c96a2
Diana Rosado <65784827+Diana-Rosado@users.noreply.github.com>
2023-06-17 19:23:08 -0400

    firebase deploy

---
+++ build/precache-manifest.78a23cb3c4bbfd4283fe670e9edb9a28.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "5db57654f803c06eebe0",
+    "url": "/static/css/main.42015092.chunk.css"
+  },
+  {
+    "revision": "5db57654f803c06eebe0",
+    "url": "/static/js/main.5db57654.chunk.js"
+  },
+  {
+    "revision": "e0c67c5be6ca1ef18f73",
+    "url": "/static/css/1.8263f8b9.chunk.css"
+  },
+  {
+    "revision": "e0c67c5be6ca1ef18f73",
+    "url": "/static/js/1.e0c67c5b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b2f8fe1e58c16ab4c485ceb8cb7713dc",
+    "url": "/static/media/logo.b2f8fe1e.png"
+  },
+  {
+    "revision": "c6108a27f5b1f9c59f80e9465e49e95b",
+    "url": "/static/media/montserrat-cyrillic-ext-400-normal.c6108a27.woff2"
+  },
+  {
+    "revision": "76beb3bfa950b8d699fb110fa1c476f9",
+    "url": "/static/media/montserrat-cyrillic-ext-400-normal.76beb3bf.woff"
+  },
+  {
+    "revision": "3c1afb27e2870e664c5ceddafea2f3a6",
+    "url": "/static/media/montserrat-cyrillic-400-normal.3c1afb27.woff"
+  },
+  {
+    "revision": "bc1039c3bfaae932e40a9b5bfeb85048",
+    "url": "/static/media/montserrat-cyrillic-400-normal.bc1039c3.woff2"
+  },
+  {
+    "revision": "34b5787beef98663cf6194a90749dae3",
+    "url": "/static/media/montserrat-vietnamese-400-normal.34b5787b.woff2"
+  },
+  {
+    "revision": "1a4090df1b7222f1bc6236f7a7f043e3",
+    "url": "/static/media/montserrat-vietnamese-400-normal.1a4090df.woff"
+  },
+  {
+    "revision": "78ac157c4f9369f0cebc66a0cf89b3b1",
+    "url": "/static/media/montserrat-latin-ext-400-normal.78ac157c.woff"
+  },
+  {
+    "revision": "21601a1a9acf15834a3a4d6e1ebecd31",
+    "url": "/static/media/montserrat-latin-ext-400-normal.21601a1a.woff2"
+  },
+  {
+    "revision": "b4a68b1e743ee317eaaf0bbadd131571",
+    "url": "/static/media/montserrat-latin-400-normal.b4a68b1e.woff2"
+  },
+  {
+    "revision": "23facbf845d56af99e5a7d8d6eb0b74e",
+    "url": "/static/media/montserrat-latin-400-normal.23facbf8.woff"
+  },
+  {
+    "revision": "78e5a88e210d04ac74f9a57785790516",
+    "url": "/static/media/Eli.78e5a88e.jpg"
+  },
+  {
+    "revision": "ebff14ea8196e6b7f067e92374d84727",
+    "url": "/static/media/Gabe.ebff14ea.jpg"
+  },
+  {
+    "revision": "1a5991834f12d4c91f5f8ca466a974bf",
+    "url": "/static/media/Koudjedji.1a599183.jpg"
+  },
+  {
+    "revision": "d4fd0b0bcbb994ce0652f8545ac801cf",
+    "url": "/static/media/Katherine.d4fd0b0b.jpg"
+  },
+  {
+    "revision": "687abeadb100bd78156c38c6f1b5dcab",
+    "url": "/static/media/Stevens.687abead.jpg"
+  },
+  {
+    "revision": "1683c1c84d1ca92cafbad3148876717c",
+    "url": "/static/media/LandingPageVideo.1683c1c8.gif"
+  },
+  {
+    "revision": "2a3d47439c0491ca9c4af5e9e883ed8a",
+    "url": "/static/media/Waving-Craig.2a3d4743.gif"
+  },
+  {
+    "revision": "39de9bdaf1b58c576160387c9f48ef2d",
+    "url": "/static/media/Jumping-For-Joy.39de9bda.gif"
+  },
+  {
+    "revision": "3013f90945f36111c14d1b6b9c057264",
+    "url": "/static/media/look-around.3013f909.gif"
+  },
+  {
+    "revision": "f1b746f4d02a52b83cf0d940adae4f39",
+    "url": "/static/media/Diana.f1b746f4.png"
+  },
+  {
+    "revision": "a09381e27253182242399ab73c5d0ab5",
+    "url": "/static/media/Bryan.a09381e2.png"
+  },
+  {
+    "revision": "868702602e4152d0b2177c1381fed0c6",
+    "url": "/static/media/Stevens-2.86870260.jpg"
+  },
+  {
+    "revision": "f5ae4321fbd869a9d250bdd1e31a9267",
+    "url": "/index.html"
+  }
+];
