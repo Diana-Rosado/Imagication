9d76a3bf77829101d98320f596d6d4edf4d11603
Bryan Pinos <pinosbryana@gmail.com>
2023-06-19 07:31:10 -0400

    Removed unneeded comments in firebaseconfig

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,7 +1,5 @@
 // import firebase from "firebase";/
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import { getFirestore } from "firebase/firestore";
 
 var firebaseConfig = {
   apiKey: "AIzaSyBV4tKqTwWobCEqfHtyRjrP3IncoR6oiW4",
@@ -12,28 +10,7 @@ var firebaseConfig = {
   appId: "1:954970607956:web:0c6dcf599336004117f8c1",
   measurementId: "G-G425X852DH",
 };
-// Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
-// firebase.analytics();
 
-// export default firebase;
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-// const db = getFirestore(app);
-
-// const colRef = collection(db, "salesQuote");
-
-// const addSalesQuote = document.querySelector(".add");
-// addSalesQuote.addEventListener("submit", (e) => {
-//   e.preventDefault();
-
-//   addDoc(colRef, {
-//     fullName: "Bry",
-//     email: "pin",
-//     phoneNumber: "9999",
-//     schoolName: "njit",
-//     estimatedCosts: [15000, 0, 0, 4, 1],
-//   });
-// });
 
 export default app;
